b7bc50bd1a3c506127beb540e6b00be05cd4fcdb
Godsent Michael <gentlemotivator@gmail.com>
2024-04-23 09:28:42 +0100

    Done with the project

--- tests/authSpec.js
+++ tests/authSpec.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const request = require('supertest')
 const userModel = require('../model/userModel')
 const app = require('../index')
